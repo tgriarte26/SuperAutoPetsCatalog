6d3bee62499fb3db2178a12ec203d71cab80196d
trevor <trevorgriarte@gmail.com>
2026-04-18 04:44:33 -0700

    styled card

--- scripts.js
+++ scripts.js
@@ -30,12 +30,17 @@ const CURB_POSTER_URL =
 const EAST_LOS_HIGH_POSTER_URL =
   "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
+
 // This is an array of strings (TV show titles)
 let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
+  "Ant"
 ];
+
+
+let tierOnePetsImages = [
+  'tierOnePetsImages/Ant.png'
+]
+
 // Your final submission should have much more data than this, and
 // you should use more than just an array of strings to store it all.
 
@@ -47,9 +52,11 @@ function showCards() {
 
   for (let i = 0; i < titles.length; i++) {
     let title = titles[i];
+    let tierOnePetsImage = tierOnePetsImages[i]
 
     // This part of the code doesn't scale very well! After you add your
     // own data, you'll need to do something totally different here.
+    /*
     let imageURL = "";
     if (i == 0) {
       imageURL = FRESH_PRINCE_URL;
@@ -58,22 +65,26 @@ function showCards() {
     } else if (i == 2) {
       imageURL = EAST_LOS_HIGH_POSTER_URL;
     }
-
+    */
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, title, tierOnePetsImage); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+function editCardContent(card, newTitle, newImage) {
   card.style.display = "block";
 
   const cardHeader = card.querySelector("h2");
   cardHeader.textContent = newTitle;
 
   const cardImage = card.querySelector("img");
+  cardImage.src = newImage;
+
+  /*
   cardImage.src = newImageURL;
   cardImage.alt = newTitle + " Poster";
+  */
 
   // You can use console.log to help you debug!
   // View the output by right clicking on your website,
@@ -84,6 +95,7 @@ function editCardContent(card, newTitle, newImageURL) {
 // This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
 
+/*
 function quoteAlert() {
   console.log("Button Clicked!");
   alert(
@@ -95,3 +107,4 @@ function removeLastCard() {
   titles.pop(); // Remove last item in titles array
   showCards(); // Call showCards again to refresh
 }
+*/
