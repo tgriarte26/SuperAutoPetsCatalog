b9555320f5b8560edc400a63457179148863049b
trevor <trevorgriarte@gmail.com>
2026-04-19 11:34:51 -0700

    adding search bar and more cards

--- scripts.js
+++ scripts.js
@@ -22,23 +22,56 @@
  *    with the string you added to the array, but a broken image.
  *
  */
-
+/*
 const FRESH_PRINCE_URL =
   "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
 const CURB_POSTER_URL =
   "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
 const EAST_LOS_HIGH_POSTER_URL =
   "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+*/
 
-
-// This is an array of strings (TV show titles)
-let titles = [
-  "Ant"
-];
-
-
-let tierOnePetsImages = [
-  'tierOnePetsImages/Ant.png'
+const tierOnePets = [
+  {
+    name: "Ant",
+    img: "tierOnePetsImages/Ant.png",
+    tierImg: "tierDiceImages/tier1dice.png",
+  },
+  {
+    name: "Beaver",
+    img: "tierOnePetsImages/Beaver.png",
+    tierImg: "tierDiceImages/tier1dice.png",
+  },
+  {
+    name: "Cricket",
+    img: 'tierOnePetsImages/Cricket.png',
+    tierImg: "tierDiceImages/tier1dice.png",
+  },
+  {
+    name: "Duck",
+    img: 'tierOnePetsImages/Duck.png',
+    tierImg: "tierDiceImages/tier1dice.png",
+  },
+  {
+    name: "Fish",
+    img: 'tierOnePetsImages/Fish.png',
+    tierImg: "tierDiceImages/tier1dice.png",
+  },
+  {
+    name: "Horse",
+    img: 'tierOnePetsImages/Horse.png',
+    tierImg: "tierDiceImages/tier1dice.png",
+  },
+  {
+    name: "Mosquito",
+    img: 'tierOnePetsImages/Mosquito.png',
+    tierImg: "tierDiceImages/tier1dice.png",
+  },
+  {
+    name: "Otter",
+    img: 'tierOnePetsImages/Otter.png',
+    tierImg: "tierDiceImages/tier1dice.png",
+  },
 ]
 
 // Your final submission should have much more data than this, and
@@ -50,9 +83,10 @@ function showCards() {
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-    let tierOnePetsImage = tierOnePetsImages[i]
+  for (let i = 0; i < tierOnePets.length; i++) {
+    let name = tierOnePets[i].name;
+    let tierOnePetsImage = tierOnePets[i].img;
+    let tierImg = tierOnePets[i].tierImg;
 
     // This part of the code doesn't scale very well! After you add your
     // own data, you'll need to do something totally different here.
@@ -67,19 +101,22 @@ function showCards() {
     }
     */
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, tierOnePetsImage); // Edit title and image
+    editCardContent(nextCard, name, tierOnePetsImage, tierImg); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
-function editCardContent(card, newTitle, newImage) {
+function editCardContent(card, newName, newImage, newTierImg) {
   card.style.display = "block";
 
   const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
+  cardHeader.textContent = newName;
 
   const cardImage = card.querySelector("img");
   cardImage.src = newImage;
+
+  const cardTierImage = card.querySelector("#tier-img");
+  cardTierImage.src = newTierImg;
 
   /*
   cardImage.src = newImageURL;
@@ -89,7 +126,13 @@ function editCardContent(card, newTitle, newImage) {
   // You can use console.log to help you debug!
   // View the output by right clicking on your website,
   // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
+  console.log("new card:", newName, "- html: ", card);
+}
+
+const cardContainer = document.getElementById("card-container");
+
+function searchBar() {
+  const input = document.getElementById('input');
 }
 
 // This calls the addCards() function when the page is first loaded
